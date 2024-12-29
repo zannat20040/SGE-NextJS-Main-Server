097dcf32fa6b08ecaa09f55d2439ec99356e4fd5
zannat20040 <shanzidadisha1234@gmail.com>
2024-12-29 22:32:30 +0600

    blog delete api created

---
+++ controllers/deleteBlog.js
@@ -0,0 +1,19 @@
+// controllers/deleteBlog.js
+const Blog = require("../models/Blog"); // Adjust the path to your Blog model
+
+const deleteBlog = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const blog = await Blog.findByIdAndDelete(id);
+
+    if (!blog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+
+    res.status(200).json({ message: "Blog deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting blog", error });
+  }
+};
+
+module.exports = deleteBlog;
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -3,9 +3,11 @@ const blogRoute = express.Router();
 const createBlog = require("../controllers/createBlog");
 const getBlogBySlug = require("../controllers/getBlogBySlug");
 const getAllBlogs = require("../controllers/getAllBlogs");
+const deleteBlog = require("../controllers/deleteBlog");
 
 blogRoute.post("/createBlog", createBlog);
 blogRoute.get("/getAllBlogs", getAllBlogs);
 blogRoute.get("/getBlogBySlug/:slug", getBlogBySlug);
+blogRoute.delete("/deleteBlog/:id", deleteBlog);
 
 module.exports = blogRoute;
